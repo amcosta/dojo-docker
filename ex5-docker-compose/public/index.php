45ccda9a404b6993b9656f3d066fdf1eb59f1838
Alex Moreno <alex.moreno.costa@gmail.com>
2019-03-18 13:31:39 -0300

    Trabalhando no exemplo 5

--- ex5-docker-compose/public/index.php
+++ ex5-docker-compose/public/index.php
@@ -1,3 +1,15 @@
 <?php
 
+require __DIR__ . "/../vendor/autoload.php";
+
+use App\ViaCep\Client;
+
+$cep = $_GET['cep'];
+
+$client = new Client();
+$data = $client->search($cep);
+
+var_dump($data);
+
+die;
 phpinfo();
---
+++ ex5-docker-compose/src/SearchCep.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class SearchCep
+{
+    public function __construct()
+    {
+
+    }
+}
